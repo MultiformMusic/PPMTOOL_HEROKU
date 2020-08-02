3903b43609020da84a2ed31f5cc11130e7cfb57d
mdio <michel.dio33@gmail.com>
2020-08-02 15:35:11 +0200

    heroku react

---
+++ src/main/resources/static/precache-manifest.ec63f20f0387cdff1fdf9460e4e62933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a360012e815bd712d682e4a1e5f02f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d414b82fec1c9c0e261b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c401d1e695abdb569c24",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "d414b82fec1c9c0e261b",
+    "url": "/static/js/2.6c38ba06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6c38ba06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c401d1e695abdb569c24",
+    "url": "/static/js/main.49267943.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
